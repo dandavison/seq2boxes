fee0d19224257b6a3e58abf234b502fa8b3c37b7
Dan Davison <dan.davison@temporal.io>
2025-08-30 10:00:24 -0400

    Use stdin/stdout, --double-dashes

--- README.md
+++ README.md
@@ -11,19 +11,33 @@ go build -o seq2boxes
 ## Usage
 
 ```
-./seq2boxes -i sequence.d2
-./seq2boxes -i sequence.d2 -o output.d2 -layout horizontal -arrows simple
+./seq2boxes [options] <input-file>
+./seq2boxes [options] -
+cat sequence.d2 | ./seq2boxes -
 ```
 
 ## Options
 
-- `-i, --input`: Input sequence diagram file (required)
-- `-o, --output`: Output file (default: input-boxes.d2)
 - `--layout`: vertical (default) or horizontal
 - `--arrows`: simple or detailed (default)
 - `--theme`: D2 theme name
-- `-v, --verbose`: Verbose output
-- `--help`: Show help
+- `--verbose`: Verbose output to stderr
+
+## Examples
+
+```
+# Basic conversion to stdout
+./seq2boxes diagram.d2
+
+# Horizontal layout with simple arrows
+./seq2boxes --layout horizontal --arrows simple diagram.d2
+
+# From stdin with terminal theme
+cat diagram.d2 | ./seq2boxes --theme terminal -
+
+# Save to file
+./seq2boxes diagram.d2 > output.d2
+```
 
 ## Arrow Modes
 
--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
-	"path/filepath"
 )
 
 type Config struct {
-	InputFile  string
-	OutputFile string
-	Layout     string
-	ArrowMode  string
-	Theme      string
-	Verbose    bool
+	InputFile string
+	Layout    string
+	ArrowMode string
+	Theme     string
+	Verbose   bool
 }
 
 func main() {
@@ -28,60 +27,61 @@ func main() {
 func parseFlags() Config {
 	var config Config
 
-	flag.StringVar(&config.InputFile, "input", "", "Input D2 sequence diagram file (required)")
-	flag.StringVar(&config.InputFile, "i", "", "Input D2 sequence diagram file (short form)")
-	flag.StringVar(&config.OutputFile, "output", "", "Output D2 boxes and arrows diagram file (default: input name with -boxes suffix)")
-	flag.StringVar(&config.OutputFile, "o", "", "Output D2 boxes and arrows diagram file (short form)")
 	flag.StringVar(&config.Layout, "layout", "vertical", "Layout direction: vertical or horizontal")
 	flag.StringVar(&config.ArrowMode, "arrows", "detailed", "Arrow mode: simple (single arrow) or detailed (numbered with colors)")
 	flag.StringVar(&config.Theme, "theme", "neutral-default", "D2 theme to use")
-	flag.BoolVar(&config.Verbose, "verbose", false, "Enable verbose output")
-	flag.BoolVar(&config.Verbose, "v", false, "Enable verbose output (short form)")
+	flag.BoolVar(&config.Verbose, "verbose", false, "Enable verbose output to stderr")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "seq2boxes - Convert D2 sequence diagrams to boxes and arrows diagrams\n\n")
 		fmt.Fprintf(os.Stderr, "Usage:\n")
-		fmt.Fprintf(os.Stderr, "  %s [options]\n\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s [options] <input-file>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s [options] -\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  cat file.d2 | %s [options] -\n\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Arguments:\n")
+		fmt.Fprintf(os.Stderr, "  <input-file>    Input D2 sequence diagram file (use '-' for stdin)\n\n")
 		fmt.Fprintf(os.Stderr, "Options:\n")
 		flag.PrintDefaults()
 		fmt.Fprintf(os.Stderr, "\nExamples:\n")
-		fmt.Fprintf(os.Stderr, "  %s -i diagram.d2\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "  %s -i diagram.d2 -o output.d2 -layout horizontal\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "  %s -i diagram.d2 -arrows simple\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s diagram.d2\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s --layout horizontal --arrows simple diagram.d2\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  cat diagram.d2 | %s --theme terminal -\n", os.Args[0])
 	}
 
 	flag.Parse()
 
-	if config.InputFile == "" {
+	// Get positional argument
+	args := flag.Args()
+	if len(args) != 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	if config.OutputFile == "" {
-		config.OutputFile = generateOutputFilename(config.InputFile)
-	}
+	config.InputFile = args[0]
 
 	return config
 }
 
-func generateOutputFilename(inputFile string) string {
-	dir := filepath.Dir(inputFile)
-	base := filepath.Base(inputFile)
-	ext := filepath.Ext(base)
-	name := base[:len(base)-len(ext)]
-	return filepath.Join(dir, name+"-boxes"+ext)
-}
-
 func run(config Config) error {
 	if config.Verbose {
-		fmt.Printf("Converting %s to %s\n", config.InputFile, config.OutputFile)
-		fmt.Printf("Layout: %s, Arrow mode: %s\n", config.Layout, config.ArrowMode)
+		fmt.Fprintf(os.Stderr, "Converting %s\n", config.InputFile)
+		fmt.Fprintf(os.Stderr, "Layout: %s, Arrow mode: %s\n", config.Layout, config.ArrowMode)
 	}
 
-	// Read input file
-	input, err := os.ReadFile(config.InputFile)
-	if err != nil {
-		return fmt.Errorf("failed to read input file: %w", err)
+	// Read input
+	var input []byte
+	var err error
+
+	if config.InputFile == "-" {
+		input, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			return fmt.Errorf("failed to read from stdin: %w", err)
+		}
+	} else {
+		input, err = os.ReadFile(config.InputFile)
+		if err != nil {
+			return fmt.Errorf("failed to read input file: %w", err)
+		}
 	}
 
 	// Parse sequence diagram
@@ -96,13 +96,11 @@ func run(config Config) error {
 		return fmt.Errorf("failed to convert diagram: %w", err)
 	}
 
-	// Write output file
-	if err := os.WriteFile(config.OutputFile, []byte(boxesAndArrows), 0644); err != nil {
-		return fmt.Errorf("failed to write output file: %w", err)
-	}
+	// Write to stdout
+	fmt.Print(boxesAndArrows)
 
 	if config.Verbose {
-		fmt.Printf("Successfully created %s\n", config.OutputFile)
+		fmt.Fprintf(os.Stderr, "Conversion complete\n")
 	}
 
 	return nil
